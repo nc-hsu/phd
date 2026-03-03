7e5d3424ae57fc7d314cfc1a09bee961d5819155
Nicholas Clemett <nicholas.clemett@hsu-hh.de>
2026-03-03 08:35:08 +0100

    Update testtables for correlation models used in OQ AvgSA GMPE

--- phd_project/scripts/WP1_ground_motion_set/generate_oq_gmpe_testtables.py
+++ phd_project/scripts/WP1_ground_motion_set/generate_oq_gmpe_testtables.py
@@ -4,7 +4,7 @@ Script for generating test tables for the Indirect AvgSA GMPES in OpenQuake
 
 import numpy as np
 import pandas as pd
-from openquake.hazardlib.imt import SA
+from openquake.hazardlib.imt import SA, PGA
 from openquake.hazardlib.gsim.kotha_2020 import KothaEtAl2020ESHM20
 from phd_project.config.config import load_config
 
@@ -14,11 +14,22 @@ folder = cfg["proc_data"]["corr_model"] / "test_tables_for_openquake"
 
 ## Testtables for GenericGMPEAvgSaTablesTestCaseClemettAsc
 # periods for the generic GMPE: 
-ts = [0.3, 0.4, 0.5]
+ts1 = [0.3, 0.4, 0.5]
+ts2 = [0.0, 0.1, 0.2]
+
+# correlations:
+rhos_ts1 = np.array(
+    [[1.000, 0.943, 0.890],
+     [0.943, 1.000, 0.958],
+     [0.890, 0.958, 1.000]])
+
+rhos_ts2 = np.array(
+    [[1.000, 0.955, 0.943],
+     [0.955, 1.000, 0.909],
+     [0.943, 0.909, 1.000]])
 
 # create the dataframe of the input parameters
 parameters = {}
-
 parameters["rup_mag"] = np.array(sorted(5 * [4.5, 6.0, 7.5]))
 parameters["rup_hypo_depth"] = 10 * np.ones(15)
 parameters["dist_rjb"] = np.array(3 * [0, 50, 100, 150, 200])
@@ -30,11 +41,6 @@ parameters["damping"] = 5 * np.ones(15)
 testtable = pd.DataFrame.from_dict(parameters, orient="columns")
 
 # calculate AvgSA and the total sigma
-rhos = np.array(
-    [[1.000, 0.943, 0.890],
-     [0.943, 1.000, 0.958],
-     [0.890, 0.958, 1.000]])
-
 site_rup_ctxs = np.recarray(
     len(parameters["rup_mag"]), 
     dtype=[
@@ -52,7 +58,8 @@ site_rup_ctxs["region"] = parameters["site_region"]
 site_rup_ctxs["vs30"] = parameters["site_vs30"]
 site_rup_ctxs["vs30measured"] = parameters["site_vs30measured"]
 
-imts = [SA(t) for t in ts]
+## Case 1: ts1 -> just SA
+imts = [SA(t) for t in ts1]
 mean = np.zeros((len(imts), len(site_rup_ctxs)))
 sigma = np.zeros_like(mean)
 tau = np.zeros_like(mean)
@@ -62,7 +69,7 @@ KothaEtAl2020ESHM20().compute(site_rup_ctxs, imts, mean, sigma, tau, phi)
 
 avgSA_mean = np.exp(mean.mean(axis=0))
 avgSA_sigma = np.sqrt(
-    (rhos[:, np.newaxis, :] * np.einsum("ij, kj -> ijk", sigma, sigma)) \
+    (rhos_ts1[:, np.newaxis, :] * np.einsum("ij, kj -> ijk", sigma, sigma)) \
     .sum(axis=(0,2))) / len(imts)
 
 testtable_avgSA_mean = testtable.copy()
@@ -75,6 +82,31 @@ testtable_avgSA_stddev["AvgSA"] = avgSA_sigma
 
 testtable_avgSA_mean.to_csv(folder / "generic_gmpe_avgsa_clemettasc_mean.csv")
 testtable_avgSA_stddev.to_csv(folder / "generic_gmpe_avgsa_clemettasc_stddev.csv")
+
+## Case 2: ts2 -> SA and PGA
+imts = [SA(t) if t != 0 else PGA() for t in ts2]
+mean = np.zeros((len(imts), len(site_rup_ctxs)))
+sigma = np.zeros_like(mean)
+tau = np.zeros_like(mean)
+phi = np.zeros_like(mean)
+
+KothaEtAl2020ESHM20().compute(site_rup_ctxs, imts, mean, sigma, tau, phi)
+
+avgSA_mean = np.exp(mean.mean(axis=0))
+avgSA_sigma = np.sqrt(
+    (rhos_ts2[:, np.newaxis, :] * np.einsum("ij, kj -> ijk", sigma, sigma)) \
+    .sum(axis=(0,2))) / len(imts)
+
+testtable_avgSA_mean = testtable.copy()
+testtable_avgSA_mean.loc[:, "result_type"] = "MEAN"
+testtable_avgSA_mean["AvgSA"] = avgSA_mean
+
+testtable_avgSA_stddev = testtable.copy()
+testtable_avgSA_stddev.loc[:, "result_type"] = "TOTAL_STDDEV"
+testtable_avgSA_stddev["AvgSA"] = avgSA_sigma
+
+testtable_avgSA_mean.to_csv(folder / "generic_gmpe_avgsa_clemettasc_w_pga_mean.csv")
+testtable_avgSA_stddev.to_csv(folder / "generic_gmpe_avgsa_clemettasc_w_pga_stddev.csv")
 
 if __name__ == "__main__":
     ...
